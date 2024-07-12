7d58b888ea2ba462e544a19497a52979a1c5292b
Carlos Cazelatto Jr <carlosjunior1983@gmail.com>
2024-07-12 15:42:55 -0300

    chore: start user-service-api

---
+++ user-service-api/src/test/java/br/com/carlosjunior/userserviceapi/UserServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carlosjunior.userserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

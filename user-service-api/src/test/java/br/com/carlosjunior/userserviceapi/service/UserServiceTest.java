57916d62ca0e09dab8d64af7a1ad06932c97dc9f
Carlos Cazelatto Jr <carlosjunior1983@gmail.com>
2024-07-15 11:06:36 -0300

    test: add test to save  user service with exception

--- user-service-api/src/test/java/br/com/carlosjunior/userserviceapi/service/UserServiceTest.java
+++ user-service-api/src/test/java/br/com/carlosjunior/userserviceapi/service/UserServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.List;
@@ -99,6 +100,25 @@ class UserServiceTest {
         verify(passwordEncoder).encode(request.password());
         verify(repository).save(any(User.class));
         verify(repository).findByEmail(request.email());
+    }
+
+    @Test
+    void whenCallSaveWithInvalidEmailThenThrowDataIntegrityViolationException() {
+        final var request = generateMock(CreateUserRequest.class);
+        final var entity = generateMock(User.class);
+
+        when(repository.findByEmail(anyString())).thenReturn(Optional.of(entity));
+
+        try {
+            service.save(request);
+        } catch (Exception e) {
+            assertEquals(DataIntegrityViolationException.class, e.getClass());
+            assertEquals("E-mail [ " + request.email() + "] already exists", e.getMessage());
+        }
+        verify(repository).findByEmail(request.email());
+        verify(mapper, times(0)).fromRequest(request);
+        verify(passwordEncoder, times(0)).encode(request.password());
+        verify(repository, times(0)).save(any(User.class));
     }
 
 }

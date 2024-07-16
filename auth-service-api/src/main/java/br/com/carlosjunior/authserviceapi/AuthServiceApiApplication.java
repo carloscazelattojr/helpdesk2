bdefafa0dbbbdd2d2365fa3653356b74f9aacf29
Carlos Cazelatto Jr <carlosjunior1983@gmail.com>
2024-07-15 22:49:31 -0300

    chore: starter auth-service-api

---
+++ auth-service-api/src/main/java/br/com/carlosjunior/authserviceapi/AuthServiceApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.carlosjunior.authserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApiApplication.class, args);
+    }
+
+}
---
+++ auth-service-api/src/test/java/br/com/carlosjunior/authserviceapi/AuthServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carlosjunior.authserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
